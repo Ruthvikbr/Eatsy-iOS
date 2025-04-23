66cb9b7287babb8a4c63a8c65d8a12acc757fc97
Ruthvikbr <ruthvikbr24@gmail.com>
2025-04-23 18:50:22 -0400

    Initial Commit

---
+++ Eatsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eatsy
+//
+//  Created by Ruthvik Ravi on 4/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
